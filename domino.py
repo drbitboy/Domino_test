df5a2c93d0c4a8e935e23bc5ec3e95334d32deab
Brian Carcich <briantcarcich@gmail.com>
2021-05-19 13:23:17 -0400

    Refactor and fix checksum calculation; make server validate checksum

--- domino.py
+++ domino.py
@@ -4,17 +4,26 @@
 
 STX,LF,CR,ETB,SOH,ETX = map(chr,(0x02,0x0a,0x0d,0x17,0x01,0x03,))
 
+def calculate_checksum_string(pre_checksum):
+  return '{0:02X}'.format(sum([b for b in pre_checksum.encode(encoding='cp850')]))[-2:]
+
 def encode_data_850(layout,vardata_sequence):
   vardata = LF.join(vardata_sequence)
   pre_checksum = f"{STX}041C1E{layout}Q1{ETB}D{vardata}"
-  checksum = '{0:02X}'.format(sum([ord(s) for s in pre_checksum[1:]]))[-2:]
-  return (f"{pre_checksum}{checksum}{CR}").encode('cp850')
+  checksum = calculate_checksum_string(pre_checksum[1:])
+  return (f"{pre_checksum}{checksum}{CR}").encode(encoding='cp850')
 
 def decode_data_850(encoded_string):
-  return encoded_string.decode('cp850').replace(STX,'<STX>'
-                                      ).replace(LF,'<LF>'
-                                      ).replace(CR,'<CR>'
-                                      ).replace(ETB,'<ETB>'
-                                      ).replace(SOH,'<SOH>'
-                                      ).replace(ETX,'<ETX>'
-                                      )
+  return encoded_string.decode(encoding='cp850').replace(STX,'<STX>'
+                                               ).replace(LF,'<LF>'
+                                               ).replace(CR,'<CR>'
+                                               ).replace(ETB,'<ETB>'
+                                               ).replace(SOH,'<SOH>'
+                                               ).replace(ETX,'<ETX>'
+                                               )
+
+def checksum_is_valid(encoded_data):
+  source_checksum = encoded_data[-3:-1].decode(encoding='cp850')
+  calculated_checksum = calculate_checksum_string(encoded_data[1:-3].decode(encoding='cp850'))
+  return source_checksum == calculated_checksum,source_checksum,calculated_checksum
+  
--- dominoserver.py
+++ dominoserver.py
@@ -4,10 +4,17 @@ from domino import *
 ### Server
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)               ### Create a TCP/IP socket
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)             ### Keep TIME_WAIT socket from hanging
-sock.bind(('',9100,))                                         ### Bind socket to port 9100
+sock.bind(('',9100,))                                                  ### Bind socket to port 9100
 sock.listen(1)                                                         ### Listen on that port
 connection,client_address = sock.accept()                              ### Accept client connection
-data = connection.recv(1024)                                           ### Receive client data
-print('Client data [{0}] received from {1}'.format(decode_data_850(data),client_address))
+encoded_data = connection.recv(1024)                                   ### Receive client data
+civ,src,calc = checksum_is_valid(encoded_data)
+print('Client data [{0}] received from {1}; checksum {2} valid {3}/{4}'.format(
+       decode_data_850(encoded_data)
+      ,client_address
+      ,civ and 'is' or 'is not'
+      ,src
+      ,calc
+      ,))
 connection.sendall(b'This is the server')                              ### Send server data
 connection.close()                                                     ### Close connection and exit
